576c4dbdd674f1aedf16644f73e931c50033107e
MARCO NASSABAIN <marco.nassabain@hotmail.com>
2019-12-15 15:58:54 +0100

    updated example main with free_ast

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+    printf("## test build tree ##\n");
     ast a = create_ast();
     a = add_child(a, "racine");
     a = add_child(add_child(a, "fils1"), "fils2");
@@ -16,6 +17,12 @@ int main()
     a->first_child = add_child(a->first_child, "fils9 du fils1");
     print_ast(a);
 
-    printf("child 2 is %s\n", get_name(get_children(a)[1]));
+    printf("\n## test get children ##\n");
+    ast* children = get_children(a->first_child);
+    printf("child 4 of racine is %s\n", get_name(children[3]));
+
+    free(children);
+    free_ast(a);
+
     return 0;
 }
