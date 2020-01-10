5442df1661ffc8eeba205a6a716a6fc55bb3fcf9
CHRISTOFFEL Quentin <quentin.christoffel@etu.unistra.fr>
2020-01-10 23:18:49 +0100

    modif gitignore, makefile pour debug, correction tests

---
+++ src/test.c
@@ -0,0 +1,42 @@
+#define DEF 4
+#include <stdio.h>
+
+// ce commentaire aussi
+
+int main() {
+
+    /* ce commentaire est ignoré */
+
+    int constante = 5; // et lui aussi
+    constante = 4;
+    double non = 5.4, oui, yes;
+
+    if ( (5 > 2 || 6 > 4) && a == b) {
+        a = 5;
+        b = -4e-5;
+    } else {
+        b = 3;
+        if ( a >= b ) {
+            double c = 4.5;
+        }
+        while ( i <= 10) {
+            a = -b;
+            while (j != 4) {
+                b = (c + 49.4) / 32.2 * 6;
+            }
+        }
+    }
+    printf("%s", bonjour); printf("%d non", aozidna);
+
+    for (i = 0, j = 5, y = -45; i < 25 ; i++, j--, k++, oui = oui+5) {
+        a = a * 5 + 7;
+    }
+
+    int table[5][6][7];
+    int tableau [10] = { 4,5,6 };
+
+    int tab[2][3][4] = { { {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4} },
+                     { {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4 } } };
+
+    return -4;
+}
